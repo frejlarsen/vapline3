a2ff1c564aaf4d933259ad6716acc44f9194bfdd
frejlarsen <frejnohrlarsen@yahoo.com>
2024-01-10 15:36:39 +0100

    Update run_qualification.sh

--- run_qualification.sh
+++ run_qualification.sh
@@ -11,12 +11,14 @@ module load seqkit/0.13.2
 # Note that some of these programs might interfere with eachother. If experiencing issues, try purging/changing your environment.
 #
 # Below is a custom criteria selection from the outputs of each software. Your preferences may wary depending on how conservative you wish your virome to be.
+#
+# Before running make sure filepaths are correct for checkV, VIBRANT, and VirSorter2
 
 
-checkv end_to_end h_qual_contigs.fa -d /home/projects/cu_10168/people/frenoh/databases/VIROME_TOOL_checkv/checkv-db-v1.4/ QC_CHECKV -t 22
-VIBRANT_run.py -i h_qual_contigs.fa -t 22 -virome -d /home/projects/cu_10168/people/frenoh/databases/VIROME_TOOL_vibrant/databases/ -m /home/projects/cu_10168/people/frenoh/databases/VIROME_TOOL_vibrant/files/ -folder QC_VIBRANT
+checkv end_to_end h_qual_contigs.fa -d <FILE_PATH_TO_DATABASE> QC_CHECKV -t 22
+VIBRANT_run.py -i h_qual_contigs.fa -t 22 -virome -d <FILE_PATH_TO_DATABASE> -m <FILE_PATH_TO_FILES> -folder QC_VIBRANT
 VirBot.py --input h_qual_contigs.fa --output QC_VIRBOT
-virsorter run -d /home/projects/cu_10168/people/frenoh/databases/VIROME_TOOL_virsorter2/VirSorter2/db -w QC_VIRSORTER2 -i h_qual_contigs.fa -j 22 --include-groups dsDNAphage,NCLDV,RNA,ssDNA,lavidaviridae
+virsorter run -d <FILE_PATH_TO_DATABASE> -w QC_VIRSORTER2 -i h_qual_contigs.fa -j 22 --include-groups dsDNAphage,NCLDV,RNA,ssDNA,lavidaviridae
 
 
 cat QC_CHECKV/quality_summary.tsv | cut -f 1,8 | awk '{ if ($2=="High-quality" || $2=="Medium-quality" || $2=="Complete") print }' | cut -f 1 | sed 's/_fragment/*/g' | sed 's/*/'"$TAB"'/g' | cut -f 1 > sQC_CHECKV.txt
