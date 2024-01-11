c0692a66fbfb53ab5208e951f85d26e7888dfdc1
frejlarsen <frejnohrlarsen@yahoo.com>
2024-01-11 14:04:06 +0100

    Update 06_quantify-reads.sh

--- 06_quantify-reads.sh
+++ 06_quantify-reads.sh
@@ -10,19 +10,17 @@ bowtie2-build -f h_qual_clean_contigs.fa dbname
 for file in reads*.fa
 do
 echo $file
-TAB=$'\t'
 bowtie2 -x dbname -f $file -p 24 |  samtools view -bS - > ${file}_tmp
 samtools sort ${file}_tmp  -o ${file}.bam -@ 24
 samtools index ${file}.bam
-echo 'vOTU'"${TAB}"''"${file}"'' > ${file}_header
+echo -e 'vOTU\t'"${file}"'' > ${file}_header
 samtools idxstats ${file}.bam | cut -f1,3 | sed '/*/d' > ${file}_tmp
 cat ${file}_header ${file}_tmp > ${file}.stats
 done
 
 
 seqkit fx2tab -l -n h_qual_clean_contigs.fa > tmp1
-TAB=$'\t'
-echo 'vOTU'"${TAB}"'size' > tmp2
+echo 'vOTU\tsize' > tmp2
 cat tmp2 tmp1 > h_qual_clean_contigs_sizes.txt
 
 
